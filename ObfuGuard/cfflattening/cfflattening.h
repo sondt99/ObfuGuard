b72d3f05905498613487c67f8c04864d559cf2bd
krixov <87920408+sondt99@users.noreply.github.com>
2025-06-23 11:54:16 +0700

    rm is_jmp_conditional in header file (CFF namespace)

--- ObfuGuard/cfflattening/cfflattening.h
+++ ObfuGuard/cfflattening/cfflattening.h
@@ -1,11 +1,8 @@
 #pragma once
-
 #include "../obfuscatecff/obfuscatecff.h"
 #include <vector>
 
-
 class obfuscatecff;
-
 namespace CFF {
 
     struct BasicBlock {
@@ -15,10 +12,8 @@ namespace CFF {
         int dst_block = -1;
     };
 
-    bool is_jmp_conditional(ZydisDecodedInstruction instr);
     bool apply_control_flow_flattening(std::vector<obfuscatecff::function_t>::iterator& func_iter);
 }
-
 
 namespace obfuscatecff_extensions {
     bool apply_control_flow_flattening(std::vector<obfuscatecff::function_t>::iterator& func);
