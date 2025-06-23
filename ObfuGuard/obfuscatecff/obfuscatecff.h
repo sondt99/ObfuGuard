108a877facf72bbd8b49ef20759c62742b0bd1a8
krixov <87920408+sondt99@users.noreply.github.com>
2025-06-23 13:37:11 +0700

    add comment to clarify complex parts of obfuscatecff.h

--- ObfuGuard/obfuscatecff/obfuscatecff.h
+++ ObfuGuard/obfuscatecff/obfuscatecff.h
@@ -9,108 +9,121 @@
 #include <asmjit/asmjit.h>
 using namespace asmjit;
 
-class obfuscatecff {
+class obfuscatecff { // Lớp thực hiện làm rối luồng điều khiển (Control Flow Flattening)
 private:
-	struct instruction_t;
-	struct function_t;
-	pe64* pe;
-	struct func_id_instr_id {
-		int func_id;
-		int inst_index;
-	};
-	std::map<uint64_t, func_id_instr_id> runtime_addr_track;
+    struct instruction_t; // Cấu trúc mô tả một lệnh máy
+	struct function_t; // Cấu trúc mô tả một hàm
 
-	static int instruction_id;
-	static int function_iterator;
+    pe64* pe;
+    struct func_id_instr_id {
+        int func_id;
+        int inst_index;
+    };
+    // map theo dõi địa chỉ thời gian chạy -> lệnh
+    std::map<uint64_t, func_id_instr_id> runtime_addr_track;
 
-	static std::unordered_map<ZydisRegister_, x86::Gp>lookupmap;
+    static int instruction_id;
+    static int function_iterator;
 
-	JitRuntime rt;
-	CodeHolder code;
-	x86::Assembler assm;
+    static std::unordered_map<ZydisRegister_, x86::Gp> lookupmap;
 
-	std::vector<function_t>functions;
+    //sinh mã bằng asmjit 
+    JitRuntime rt;
+    CodeHolder code;
+    x86::Assembler assm;
 
-	uint32_t total_size_used;
+    std::vector<function_t> functions;
 
+    uint32_t total_size_used;
 
-	bool find_inst_at_dst(uint64_t dst, instruction_t** instptr, function_t** funcptr);
+    bool find_inst_at_dst(uint64_t dst, instruction_t** instptr, function_t** funcptr);
 
-	void remove_jumptables();
+    void remove_jumptables();
 
-	bool analyze_functions();
+    bool analyze_functions();
 
-	void relocate(PIMAGE_SECTION_HEADER new_section);
+    void relocate(PIMAGE_SECTION_HEADER new_section);
 
-	bool find_instruction_by_id(int funcid, int instid, instruction_t* inst);
+    bool find_instruction_by_id(int funcid, int instid, instruction_t* inst);
 
-	bool fix_relative_jmps(function_t* func);
+    bool fix_relative_jmps(function_t* func);
 
-	bool convert_relative_jmps();
+    bool convert_relative_jmps();
 
-	bool apply_relocations(PIMAGE_SECTION_HEADER new_section);
+    bool apply_relocations(PIMAGE_SECTION_HEADER new_section);
 
-	void compile(PIMAGE_SECTION_HEADER new_section);
+    void compile(PIMAGE_SECTION_HEADER new_section);
 
-	std::vector<instruction_t>instructions_from_jit(uint8_t* code, uint32_t size);
+    // Dịch ngược mã máy vừa sinh ra thành instruction_t để xử lý tiếp
+    std::vector<instruction_t> instructions_from_jit(uint8_t* code, uint32_t size);
 
+    // Áp dụng kỹ thuật control flow flattening cho một hàm
+    bool apply_control_flow_flattening(std::vector<obfuscatecff::function_t>::iterator& func_iter);
 
-	bool apply_control_flow_flattening(std::vector<obfuscatecff::function_t>::iterator& func_iter);
-
-	__declspec(safebuffers)  int custom_main(int argc, char* argv[]);
+    __declspec(safebuffers) int custom_main(int argc, char* argv[]);
 
 public:
+    obfuscatecff(pe64* pe);
 
-	obfuscatecff(pe64* pe);
+    void create_functions(std::vector<pdbparser::sym_func> functions);
 
-	void create_functions(std::vector<pdbparser::sym_func>functions);
+    void run(PIMAGE_SECTION_HEADER new_section, bool obfuscate_entry_point);
 
-	void run(PIMAGE_SECTION_HEADER new_section, bool obfuscate_entry_point);
+    uint32_t get_added_size();
 
-	uint32_t get_added_size();
+    struct instruction_t {
+        int inst_id;
+        int func_id;
+        bool is_first_instruction;
+        std::vector<uint8_t> raw_bytes;
+        uint64_t runtime_address;
+        uint64_t relocated_address;
+        ZydisDisassembledInstruction zyinstr;
+        bool has_relative;
+        bool isjmpcall;
 
-	struct instruction_t {
+        struct {
+            int target_inst_id;
+            int target_func_id;
+            uint32_t offset;
+            uint32_t size;
+        } relative;
 
-		int inst_id;
-		int func_id;
-		bool is_first_instruction;
-		std::vector<uint8_t>raw_bytes;
-		uint64_t runtime_address;
-		uint64_t relocated_address;
-		ZydisDisassembledInstruction zyinstr;
-		bool has_relative;
-		bool isjmpcall;
+        uint64_t location_of_data;
 
-		struct {
-			int target_inst_id;
-			int target_func_id;
-			uint32_t offset;
-			uint32_t size;
-		}relative;
+        instruction_t()
+            : inst_id(0),
+            func_id(0),
+            is_first_instruction(false),
+            runtime_address(0),
+            relocated_address(0),
+            has_relative(false),
+            isjmpcall(false),
+            location_of_data(0),
+            relative{ 0, 0, 0, 0 },
+            zyinstr{} {
+        }
 
-		uint64_t location_of_data;
+        void load_relative_info(); // Lấy thông tin nhảy tương đối từ lệnh Zydis
+        void load(int funcid, std::vector<uint8_t> raw_data); // Tải từ mã thô
+        void load(int funcid, ZydisDisassembledInstruction zyinstruction, uint64_t runtime_address);
+        void reload();
+        void print();
+    };
 
+    struct function_t {
+        int func_id;
+        std::string name;
+        std::vector<instruction_t> instructions;
+        std::map<int, uint64_t> inst_id_index;
+        uint32_t offset;
+        uint32_t size;
 
-		void load_relative_info();
-		void load(int funcid, std::vector<uint8_t>raw_data);
-		void load(int funcid, ZydisDisassembledInstruction zyinstruction, uint64_t runtime_address);
-		void reload();
-		void print();
-	};
+        function_t(int func_id, std::string name, uint32_t offset, uint32_t size)
+            : func_id(func_id), name(name), offset(offset), size(size) {
+        };
 
-	struct function_t {
-		int func_id;
-		std::string name;
-		std::vector<instruction_t>instructions;
-		std::map<int, uint64_t> inst_id_index;
-		uint32_t offset;
-		uint32_t size;
-
-		function_t(int func_id, std::string name, uint32_t offset, uint32_t size) : func_id(func_id), name(name), offset(offset), size(size) {};
-
-		bool ctfflattening = true;
-		bool has_jumptables = false;
-	};
+        bool ctfflattening = true;
+        bool has_jumptables = false;
+    };
 };
-
-
